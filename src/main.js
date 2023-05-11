8735aee7c15e66c1b2967b7f0d57cf61498acc41
Vera Lavrentyeva <55912590+TwilightHunt@users.noreply.github.com>
2023-05-11 09:47:31 +0300

    build: firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import vuetify from "./plugins/vuetify";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { VueFire, VueFireAuth } from "vuefire";
+import { firebaseApp } from "./firebase";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,6 +13,13 @@ import "./assets/main.css";
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
-const app = createApp(App).use(pinia).use(router).use(vuetify);
+const app = createApp(App)
+  .use(pinia)
+  .use(router)
+  .use(vuetify)
+  .use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()],
+  });
 
 app.mount("#app");
